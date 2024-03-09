26d87901f1c81b35ce221b68a36ed0753a53627c
Chris Meysner <110308659+RobotTooth@users.noreply.github.com>
2024-03-09 10:21:38 +0000

    init commit

---
+++ public/script.js
@@ -0,0 +1,52 @@
+const tmdbKey = "785660b0a45188ddf5ba55dc0b10f821";
+const tmdbBaseUrl = "https://api.themoviedb.org/3";
+const playBtn = document.getElementById("playBtn");
+
+const getGenres = async () => {
+  const getGenreRequestEndpoint = "/genre/movie/list";
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${getGenreRequestEndpoint}${requestParams}`;
+
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      // console.log(jsonResponse);
+      const genres = jsonResponse.genres;
+      return genres;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getMovies = async () => {
+  const selectedGenre = getSelectedGenre();
+  const discoverMovieEndpoint = "/discover/movie";
+  const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}`;
+  const urlToFetch = `${tmdbBaseUrl}${discoverMovieEndpoint}${requestParams}`;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      // console.log(jsonResponse);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// getMovies();
+
+const getMovieInfo = () => {};
+
+// Gets a list of movies and ultimately displays the info of a random movie from the list
+const showRandomMovie = () => {
+  const movieInfo = document.getElementById("movieInfo");
+  if (movieInfo.childNodes.length > 0) {
+    clearCurrentMovie();
+  }
+};
+
+getGenres().then(populateGenreDropdown);
+playBtn.onclick = showRandomMovie;
